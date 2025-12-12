204a893826e5a36c388c9a7e8ac3a6d700e1f8a1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-12-12 20:33:41 +0400

    Add hash result model

---
+++ src/microservices/account/AccountMicroservice.Api/Models/General/FormatHashResult.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountMicroservice.Api.Models.General
+{
+    public class FormatHashResult
+    {
+        public byte[] PasswordHash { get; set; }
+        public byte[] Salt { get; set; }
+    }
+}
