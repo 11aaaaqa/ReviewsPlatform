b7cc4adbd5475f71a0bcacb78f4330dd98d7c798
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-12-12 20:34:03 +0400

    Add password service

---
+++ src/microservices/account/AccountMicroservice.Api/Services/Password services/IPasswordService.cs
@@ -0,0 +1,10 @@
+﻿using AccountMicroservice.Api.Models.General;
+
+namespace AccountMicroservice.Api.Services.Password_services
+{
+    public interface IPasswordService
+    {
+        FormatHashResult HashPassword(string password);
+        bool CheckPassword(byte[] originalPasswordHash, string userPassword, byte[] salt);
+    }
+}
