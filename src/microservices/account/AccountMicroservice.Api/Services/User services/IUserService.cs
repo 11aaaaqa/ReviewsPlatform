d6cd3cb859a6bc28a0dc44671bc1fc1f4fca04f1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-12-13 19:59:41 +0400

    Add user service

---
+++ src/microservices/account/AccountMicroservice.Api/Services/User services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using AccountMicroservice.Api.Models.Business;
+
+namespace AccountMicroservice.Api.Services.User_services
+{
+    public interface IUserService
+    {
+        Task<User?> GetUserByIdAsync(Guid userId);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+    }
+}
---
+++ src/microservices/account/AccountMicroservice.Api/Services/User services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using AccountMicroservice.Api.Database;
+using AccountMicroservice.Api.Models.Business;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountMicroservice.Api.Services.User_services
+{
+    public class UserService(ApplicationDbContext context) : IUserService
+    {
+        public async Task<User?> GetUserByIdAsync(Guid userId)
+            => await context.Users.SingleOrDefaultAsync(x => x.Id == userId);
+
+        public async Task<User?> GetUserByEmailAsync(string email)
+            => await context.Users.SingleOrDefaultAsync(x => x.Email == email);
+
+        public async Task AddUserAsync(User user)
+        {
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            context.Users.Update(user);
+            await context.SaveChangesAsync();
+        }
+    }
+}
