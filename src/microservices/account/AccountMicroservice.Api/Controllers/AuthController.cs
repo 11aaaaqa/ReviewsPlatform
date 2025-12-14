fc6ffce6cfa319a8007a85e20623be2e510c05bc
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-12-14 15:24:24 +0400

    Add auth and token controllers

---
+++ src/microservices/account/AccountMicroservice.Api/Controllers/AuthController.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Claims;
+using AccountMicroservice.Api.DTOs.Auth;
+using AccountMicroservice.Api.Models.Business;
+using AccountMicroservice.Api.Services.Password_services;
+using AccountMicroservice.Api.Services.Token_services;
+using AccountMicroservice.Api.Services.User_services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IPasswordService passwordService, IUserService userService, ITokenService tokenService) : ControllerBase
+    {
+        [Route("register")]
+        [HttpPost]
+        public async Task<IActionResult> RegisterAsync([FromBody] RegisterDto model)
+        {
+            var hashFormatResult = passwordService.HashPassword(model.Password);
+
+            try
+            {
+                await userService.AddUserAsync(new User
+                {
+                    Id = model.Id,
+                    Email = model.Email,
+                    UserName = model.UserName,
+                    IsEmailVerified = false,
+                    PasswordHash = hashFormatResult.PasswordHash,
+                    PasswordSalt = hashFormatResult.Salt
+                });
+            }
+            catch (Exception e) //заменить на exception, возникающий при конфликте уникальных индексов
+            {
+                return Conflict("Current user already exists");
+            }
+
+            return Ok();
+        }
+
+        [Route("login")]
+        [HttpPost]
+        public async Task<IActionResult> LoginAsync([FromBody] LoginDto model)
+        {
+            var user = await userService.GetUserByEmailAsync(model.UserNameOrEmail) 
+                       ?? await userService.GetUserByUserNameAsync(model.UserNameOrEmail);
+
+            bool checkPassword = passwordService.CheckPassword(user.PasswordHash, user.PasswordSalt, model.Password);
+
+            if (!checkPassword)
+                return Unauthorized("Incorrect password");
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+            var token = tokenService.GenerateAccessToken(claims);
+
+            user.RefreshToken = tokenService.GenerateRefreshToken();
+            user.RefreshTokenExpiryTime = DateTime.UtcNow.AddMonths(1);
+            
+            await userService.UpdateUserAsync(user);
+
+            return Ok(token);
+        }
+    }
+}
---
+++ src/microservices/account/AccountMicroservice.Api/Controllers/TokenController.cs
@@ -0,0 +1,74 @@
+﻿using System.Security.Claims;
+using AccountMicroservice.Api.DTOs.Token;
+using AccountMicroservice.Api.Services.Token_services;
+using AccountMicroservice.Api.Services.User_services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController(IUserService userService, ITokenService tokenService) : ControllerBase
+    {
+        [Route("revoke")]
+        [HttpGet]
+        public async Task<IActionResult> RevokeAsync(string userEmail)
+        {
+            var user = await userService.GetUserByEmailAsync(userEmail);
+            if (user == null) return NotFound();
+
+            user.RefreshTokenExpiryTime = new DateTime();
+            user.RefreshToken = null;
+
+            await userService.UpdateUserAsync(user);
+
+            return Ok();
+        }
+
+        [Route("refresh")]
+        [HttpPost]
+        public async Task<IActionResult> RefreshAsync([FromBody] RefreshTokenDto model)
+        {
+            var principal = tokenService.GetPrincipalFromExpiredToken(model.AccessToken);
+            string userName = principal.Identity.Name;
+
+            var user = await userService.GetUserByUserNameAsync(userName);
+
+            if (user == null || user.RefreshToken != model.RefreshToken || user.RefreshTokenExpiryTime < DateTime.UtcNow)
+                return Unauthorized();
+
+            string accessToken = tokenService.GenerateAccessToken(principal.Claims);
+
+            user.RefreshToken = tokenService.GenerateRefreshToken();
+            user.RefreshTokenExpiryTime = DateTime.UtcNow.AddMonths(1);
+
+            await userService.UpdateUserAsync(user);
+
+            return Ok(accessToken);
+        }
+
+        [Route("generate")]
+        [HttpGet]
+        public async Task<IActionResult> GenerateAccessTokenAsync(string userEmail)
+        {
+            var user = await userService.GetUserByEmailAsync(userEmail);
+            if (user == null)
+                return NotFound();
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+            string accessToken = tokenService.GenerateAccessToken(claims);
+
+            user.RefreshToken = tokenService.GenerateRefreshToken();
+            user.RefreshTokenExpiryTime = DateTime.UtcNow.AddMonths(1);
+
+            await userService.UpdateUserAsync(user);
+
+            return Ok(accessToken);
+        }
+    }
+}
