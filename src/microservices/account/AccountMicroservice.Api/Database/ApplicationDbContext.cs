2be2ad30b939372aea553c3ef0d627a66fa63772
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-12-14 15:22:58 +0400

    Add user name and email uniqueness indexes

--- src/microservices/account/AccountMicroservice.Api/Database/ApplicationDbContext.cs
+++ src/microservices/account/AccountMicroservice.Api/Database/ApplicationDbContext.cs
@@ -8,5 +8,13 @@ namespace AccountMicroservice.Api.Database
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>().HasIndex(x => x.Email).IsUnique();
+            builder.Entity<User>().HasIndex(x => x.UserName).IsUnique();
+        }
     }
 }
