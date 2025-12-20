62f4ee04d5089718ed143fdedd9abaa9b2962800
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-12-20 20:07:08 +0400

    Add DTOs

---
+++ src/microservices/account/AccountMicroservice.Api/DTOs/User/UpdateUserPasswordDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountMicroservice.Api.DTOs.User
+{
+    public class UpdateUserPasswordDto
+    {
+        public Guid UserId { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
