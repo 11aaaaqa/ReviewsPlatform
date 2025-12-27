4e3450ddb94200854f2d081395b2b8eb656aff29
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-12-27 19:25:23 +0400

    Add constants

---
+++ src/microservices/account/AccountMicroservice.Api/Constants/RoleNames.cs
@@ -0,0 +1,10 @@
+﻿namespace AccountMicroservice.Api.Constants
+{
+    public class RoleNames
+    {
+        public const string User = "User";
+        public const string Verified = "Verified";
+        public const string Admin = "Admin";
+        public const string Moderator = "Moderator";
+    }
+}
