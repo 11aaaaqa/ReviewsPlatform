21bb6596a38381c55134e7e355a1faa2b375c1e5
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-12-28 17:35:28 +0400

    Add user response model

---
+++ src/mvc/Web.MVC/Models/Api responses/Auth/UserResponse.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+
+namespace Web.MVC.Models.Api_responses.Auth
+{
+    public class UserResponse
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool IsEmailVerified { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+
+        public List<Role> Roles { get; set; } = new();
+    }
+}
