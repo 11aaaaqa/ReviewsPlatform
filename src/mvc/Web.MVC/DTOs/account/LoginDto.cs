c929922e62ba80e8947014a3d42a1483e9c110c6
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-12-28 17:34:27 +0400

    Add DTOs

---
+++ src/mvc/Web.MVC/DTOs/account/LoginDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.account
+{
+    public class LoginDto
+    {
+        [Required(ErrorMessage = "Поле \"Имя пользователя или адрес эл. почты\" обязательно")]
+        [Display(Name = "Имя пользователя или адрес эл. почты")]
+        [StringLength(200)]
+        public string UserNameOrEmail { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Пароль\" обязательно")]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Минимальная длина пароля 8 символов")]
+        public string Password { get; set; }
+
+        public string? ReturnUrl { get; set; }
+    }
+}
