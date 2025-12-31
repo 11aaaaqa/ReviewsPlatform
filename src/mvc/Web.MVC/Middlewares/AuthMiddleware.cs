72c0a645311fcface776e6c643f94479f2f83dc2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2025-12-31 18:34:20 +0400

    Update user response model namespace

--- src/mvc/Web.MVC/Middlewares/AuthMiddleware.cs
+++ src/mvc/Web.MVC/Middlewares/AuthMiddleware.cs
@@ -4,7 +4,7 @@ using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
 using Web.MVC.Constants;
-using Web.MVC.Models.Api_responses.Auth;
+using Web.MVC.Models.Api_responses.account;
 
 namespace Web.MVC.Middlewares
 {
