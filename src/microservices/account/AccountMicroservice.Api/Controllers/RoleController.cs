59dbb3453c2e839744fe8af350a3de76b7d47859
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-01 20:47:46 +0400

    Add role controller

---
+++ src/microservices/account/AccountMicroservice.Api/Controllers/RoleController.cs
@@ -0,0 +1,15 @@
+﻿using AccountMicroservice.Api.Services.Roles_services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleController(IRoleService roleService) : ControllerBase
+    {
+        [Route("all")]
+        [HttpGet]
+        public async Task<IActionResult> GetAllRoles()
+            => Ok(await roleService.GetAllRolesAsync());
+    }
+}
