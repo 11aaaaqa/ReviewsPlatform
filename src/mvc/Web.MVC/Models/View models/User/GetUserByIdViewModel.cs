c2847f9d8ad7ec474f66a1dafaa48b92728a6eb2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-03 17:13:07 +0400

    Add view model

---
+++ src/mvc/Web.MVC/Models/View models/User/GetUserByIdViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Web.MVC.Models.Api_responses.account;
+
+namespace Web.MVC.Models.View_models.User
+{
+    public class GetUserByIdViewModel
+    {
+        public UserResponse User { get; set; }
+        public bool CanUserSetTheRoles { get; set; }
+        public bool CanUserViewTheRoles { get; set; }
+        public List<RoleResponse> AllRoles { get; set; } = new();
+    }
+}
