ad89d0fccfff826ec6f60fa4418d837d7a225f81
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-03 17:12:29 +0400

    Update

--- src/microservices/account/AccountMicroservice.Api/Services/User services/Role services/UserRolesService.cs
+++ src/microservices/account/AccountMicroservice.Api/Services/User services/Role services/UserRolesService.cs
@@ -15,39 +15,16 @@ namespace AccountMicroservice.Api.Services.User_services.Role_services
 
         public async Task AddUserToRoleAsync(Guid userId, Guid roleId)
         {
-            var user = await context.Users.SingleOrDefaultAsync(x => x.Id == userId);
-            if (user == null)
-                throw new ArgumentException("User with current id does not exist");
-
-            var role = await context.Roles.SingleOrDefaultAsync(x => x.Id == roleId);
-            if (role == null)
-                throw new ArgumentException("Role with current id does not exist");
-
             await context.UserRoles.AddAsync(new UserRole { RoleId = roleId, UserId = userId });
             await context.SaveChangesAsync();
         }
 
         public async Task AddUserToRolesRangeAsync(Guid userId, ICollection<Guid> roleIds)
         {
-            var user = await context.Users.SingleOrDefaultAsync(x => x.Id == userId);
-            if (user == null)
-                throw new ArgumentException("User with current id does not exist");
-
-            roleIds = roleIds.Distinct().ToList();
-
-            var existingRoleIds = await context.Roles.Select(x => x.Id).ToListAsync();
-            if (roleIds.Except(existingRoleIds).Any())
-                throw new ArgumentException("Role with current id does not exist");
-
-            var userRoleIds = await context.UserRoles.Where(x => x.UserId == userId)
-                .Select(x => x.RoleId)
-                .ToListAsync();
-            var roleIdsToAdd = roleIds.Except(userRoleIds).ToList();
-
             List<UserRole> rolesToAdd = new();
-            foreach (var roleIdToAdd in roleIdsToAdd)
+            foreach (var roleIdToAdd in roleIds)
             {
-                rolesToAdd.Add(new UserRole { RoleId = roleIdToAdd, UserId = user.Id });
+                rolesToAdd.Add(new UserRole { RoleId = roleIdToAdd, UserId = userId });
             }
 
             context.UserRoles.AddRange(rolesToAdd);
@@ -56,18 +33,13 @@ namespace AccountMicroservice.Api.Services.User_services.Role_services
 
         public async Task RemoveUserRoleAsync(Guid userId, Guid roleId)
         {
-            var userRoleToRemove = await context.UserRoles.SingleOrDefaultAsync(x => x.UserId == userId && x.RoleId == roleId);
-            if (userRoleToRemove == null)
-                throw new ArgumentException("User role does not exist");
-
+            var userRoleToRemove = await context.UserRoles.SingleAsync(x => x.UserId == userId && x.RoleId == roleId);
             context.UserRoles.Remove(userRoleToRemove);
             await context.SaveChangesAsync();
         }
 
         public async Task RemoveUserRolesRangeAsync(Guid userId, ICollection<Guid> roleIds)
         {
-            roleIds = roleIds.Distinct().ToList();
-
             var userRolesToRemove = await context.UserRoles
                 .Where(x => x.UserId == userId && roleIds.Contains(x.RoleId))
                 .ToListAsync();
