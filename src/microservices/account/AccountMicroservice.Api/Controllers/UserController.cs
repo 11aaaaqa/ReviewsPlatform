94af892850b34f013a3f3adcffeea203665a7bd4
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-03 17:12:44 +0400

    Add setting user roles method

--- src/microservices/account/AccountMicroservice.Api/Controllers/UserController.cs
+++ src/microservices/account/AccountMicroservice.Api/Controllers/UserController.cs
@@ -1,14 +1,18 @@
 ﻿using System.Text;
 using AccountMicroservice.Api.DTOs.User;
+using AccountMicroservice.Api.Models.Business;
 using AccountMicroservice.Api.Services.Password_services;
+using AccountMicroservice.Api.Services.Roles_services;
 using AccountMicroservice.Api.Services.User_services;
+using AccountMicroservice.Api.Services.User_services.Role_services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccountMicroservice.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController(IUserService userService, IPasswordService passwordService) : ControllerBase
+    public class UserController(IUserService userService, IPasswordService passwordService, IUserRolesService userRoleService,
+        IRoleService roleService) : ControllerBase
     {
         [HttpGet]
         [Route("get-user-by-id/{userId}")]
@@ -49,6 +53,38 @@ namespace AccountMicroservice.Api.Controllers
             user.PasswordSalt = Encoding.UTF8.GetString(passwordHashFormatResult.Salt);
 
             await userService.UpdateUserAsync(user);
+
+            return Ok();
+        }
+
+        [Route("set-user-roles")]
+        [HttpPost]
+        public async Task<IActionResult> SetUserRolesAsync([FromBody] SetUserRolesDto model)
+        {
+            var user = await userService.GetUserByIdAsync(model.UserId);
+            if (user == null)
+                return NotFound("User not found");
+
+            model.RoleIds = model.RoleIds.Distinct().ToList();
+
+            List<Role> allRoles = await roleService.GetAllRolesAsync();
+            List<Guid> allRoleIds = allRoles.Select(x => x.Id).ToList();
+            if (model.RoleIds.Except(allRoleIds).Any())
+                return BadRequest("Role with current id does not exist");
+
+            var currentUserRoles = await userRoleService.GetUserRolesAsync(model.UserId);
+            if (currentUserRoles.Count == 0)
+            {
+                await userRoleService.AddUserToRolesRangeAsync(model.UserId, model.RoleIds);
+            }
+            else
+            {
+                List<Guid> currentUserRoleIds = currentUserRoles.Select(x => x.Id).ToList();
+                List<Guid> userRoleIdsToDelete = currentUserRoleIds.Except(model.RoleIds).ToList();
+                List<Guid> userRoleIdsToAdd = model.RoleIds.Except(currentUserRoleIds).ToList();
+                await userRoleService.AddUserToRolesRangeAsync(model.UserId, userRoleIdsToAdd);
+                await userRoleService.RemoveUserRolesRangeAsync(model.UserId, userRoleIdsToDelete);
+            }
 
             return Ok();
         }
