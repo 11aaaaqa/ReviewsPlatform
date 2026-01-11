6ee6ec781c0ff4a6debd35455117e9d849f54884
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-11 14:33:07 +0400

    Update roles adding

--- src/microservices/account/AccountMicroservice.Api/Database/ApplicationDbContext.cs
+++ src/microservices/account/AccountMicroservice.Api/Database/ApplicationDbContext.cs
@@ -22,10 +22,10 @@ namespace AccountMicroservice.Api.Database
             builder.Entity<Role>().HasIndex(x => x.Name).IsUnique();
 
             builder.Entity<Role>().HasData(
-                new Role{Id = new Guid("3d2dc356-2996-4616-aa2b-64ebc93e7d8f"), Name = RoleNames.User},
-                new Role{Id = new Guid("7af2e6a9-a998-4103-aeab-9ee9cf00fb0a"), Name = RoleNames.Verified},
-                new Role{Id = new Guid("32d6dd4f-15bd-4ef4-b3f4-733425778126"), Name = RoleNames.Admin},
-                new Role{Id = new Guid("5b69c81c-3a6e-4fad-b636-c9af92671a84"), Name = RoleNames.Moderator});
+                new Role{Id = new Guid(RoleIds.UserId), Name = RoleNames.User},
+                new Role{Id = new Guid(RoleIds.VerifiedId), Name = RoleNames.Verified},
+                new Role{Id = new Guid(RoleIds.AdminId), Name = RoleNames.Admin},
+                new Role{Id = new Guid(RoleIds.ModeratorId), Name = RoleNames.Moderator});
 
             builder.Entity<User>()
                 .HasMany(x => x.Roles)
