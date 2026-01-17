6e5fd37040b982f7c83ec5ca91cc621afe706707
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-17 18:46:54 +0400

    Add dto

---
+++ src/microservices/account/AccountMicroservice.Api/DTOs/User/SetUserAvatarDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountMicroservice.Api.DTOs.User
+{
+    public class SetUserAvatarDto
+    {
+        public Guid UserId { get; set; }
+        public byte[] AvatarSource { get; set; }
+    }
+}
