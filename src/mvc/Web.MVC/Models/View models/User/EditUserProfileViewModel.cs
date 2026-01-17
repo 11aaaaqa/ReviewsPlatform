c65850f73b7107184ea54efb1b494a85b82676fa
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-17 18:49:04 +0400

    Add view model

---
+++ src/mvc/Web.MVC/Models/View models/User/EditUserProfileViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.MVC.Models.View_models.User
+{
+    public class EditUserProfileViewModel
+    {
+        public Guid UserId { get; set; }
+        public string AvatarSrc { get; set; }
+        public string UserEmail { get; set; }
+    }
+}
