502da0547d0adb6b7b64ae403603d7680294d5f0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-21 12:25:43 +0400

    Add dto

---
+++ src/mvc/Web.MVC/DTOs/user/UpdateUserNameDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Web.MVC.DTOs.user
+{
+    public class UpdateUserNameDto
+    {
+        private string newUserName;
+
+        [Required(ErrorMessage = "Поле \"Имя пользователя\" обязательно")]
+        [Display(Name = "Имя пользователя")]
+        [StringLength(30)]
+        public string NewUserName
+        {
+            get => newUserName;
+            set => newUserName = value != null ? Regex.Replace(value.Trim(), @"\s+", " ") : null;
+        }
+    }
+}
