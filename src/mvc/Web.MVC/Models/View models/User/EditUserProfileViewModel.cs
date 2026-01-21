61e34c273df3ceaa90b7265ecbd250ce029de2ab
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-21 12:25:38 +0400

    Update view model

--- src/mvc/Web.MVC/Models/View models/User/EditUserProfileViewModel.cs
+++ src/mvc/Web.MVC/Models/View models/User/EditUserProfileViewModel.cs
@@ -6,5 +6,6 @@
         public bool IsAvatarDefault { get; set; }
         public string AvatarSrc { get; set; }
         public string UserEmail { get; set; }
+        public string UserName { get; set; }
     }
 }
