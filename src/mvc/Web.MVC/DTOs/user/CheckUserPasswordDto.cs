43ce5950909130558ed3d5d935887dace8cf47cc
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-21 16:41:46 +0400

    Add dtos

---
+++ src/mvc/Web.MVC/DTOs/user/CheckUserPasswordDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.user
+{
+    public class CheckUserPasswordDto
+    {
+        [Required(ErrorMessage = "Поле \"Пароль\" обязательно")]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Минимальная длина пароля 8 символов")]
+        public string Password { get; set; }
+    }
+}
---
+++ src/mvc/Web.MVC/DTOs/user/UpdateUserPasswordDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.MVC.DTOs.user
+{
+    public class UpdateUserPasswordDto
+    {
+        [Required(ErrorMessage = "Поле \"Пароль\" обязательно")]
+        [Display(Name = "Пароль")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Минимальная длина пароля 8 символов")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Подтвердите пароль\" обязательно")]
+        [Display(Name = "Подтвердите пароль")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(NewPassword), ErrorMessage = "Пароли не совпадают")]
+        [StringLength(100)]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
