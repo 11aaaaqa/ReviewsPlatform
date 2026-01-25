4de86867728ccbda135b7dce93e7c06e06ed9122
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-25 12:41:56 +0400

    Add data annotations

--- src/microservices/account/AccountMicroservice.Api/DTOs/Token/RefreshTokenDto.cs
+++ src/microservices/account/AccountMicroservice.Api/DTOs/Token/RefreshTokenDto.cs
@@ -1,8 +1,12 @@
-﻿namespace AccountMicroservice.Api.DTOs.Token
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccountMicroservice.Api.DTOs.Token
 {
     public class RefreshTokenDto
     {
+        [Required]
         public string AccessToken { get; set; }
+        [Required]
         public string RefreshToken { get; set; }
     }
 }
--- src/microservices/account/AccountMicroservice.Api/DTOs/User/UpdateUserNameDto.cs
+++ src/microservices/account/AccountMicroservice.Api/DTOs/User/UpdateUserNameDto.cs
@@ -1,8 +1,19 @@
-﻿namespace AccountMicroservice.Api.DTOs.User
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace AccountMicroservice.Api.DTOs.User
 {
     public class UpdateUserNameDto
     {
         public Guid UserId { get; set; }
-        public string NewUserName { get; set; }
+
+        private string newUserName;
+        [Required]
+        [StringLength(30)]
+        public string NewUserName
+        {
+            get => newUserName;
+            set => newUserName = value != null ? Regex.Replace(value.Trim(), @"\s+", " ") : null;
+        }
     }
 }
