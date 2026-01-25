2b20943d02872b1d7f4b04be724dc02185144edf
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-01-25 12:46:31 +0400

    Add swagger

--- src/microservices/account/AccountMicroservice.Api/Program.cs
+++ src/microservices/account/AccountMicroservice.Api/Program.cs
@@ -46,13 +46,14 @@ builder.Services.AddSingleton<ITokenService, TokenService>();
 builder.Services.AddSingleton<IAvatarService, AvatarService>();
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseAuthentication();
