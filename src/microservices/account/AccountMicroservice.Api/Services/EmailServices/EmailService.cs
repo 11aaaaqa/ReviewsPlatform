e14e36cd30a8d55b8e8ecc5d5382f572a03feecc
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-02-01 15:01:14 +0400

    Add email service

---
+++ src/microservices/account/AccountMicroservice.Api/Services/EmailServices/EmailService.cs
@@ -0,0 +1,24 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using MimeKit.Text;
+
+namespace AccountMicroservice.Api.Services.EmailServices
+{
+    public class EmailService(IConfiguration configuration) : IEmailService
+    {
+        public async Task SendEmailAsync(string toAddress, string title, string text)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(configuration["COMPANY_NAME"], configuration["EMAIL_ADDRESS"]));
+            message.To.Add(new MailboxAddress(toAddress, toAddress));
+            message.Subject = title;
+            message.Body = new TextPart(TextFormat.Html) { Text = text };
+
+            using var client = new SmtpClient();
+            await client.ConnectAsync(configuration["EMAIL_HOST"], int.Parse(configuration["EMAIL_PORT"]!), true);
+            await client.AuthenticateAsync(configuration["EMAIL_ADDRESS"], configuration["EMAIL_PASSWORD"]);
+            await client.SendAsync(message);
+            await client.DisconnectAsync(true);
+        }
+    }
+}
---
+++ src/microservices/account/AccountMicroservice.Api/Services/EmailServices/IEmailService.cs
@@ -0,0 +1,7 @@
+﻿namespace AccountMicroservice.Api.Services.EmailServices
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string toAddress, string title, string text);
+    }
+}
