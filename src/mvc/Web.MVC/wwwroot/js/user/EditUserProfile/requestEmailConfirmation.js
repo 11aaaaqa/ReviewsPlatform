2abeea7e909ad0063a6270c6990af3182b69e75a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-02-01 18:31:45 +0400

    Split scripts into files

---
+++ src/mvc/Web.MVC/wwwroot/js/user/EditUserProfile/requestEmailConfirmation.js
@@ -0,0 +1,33 @@
+﻿const confirmationWaitingP = document.getElementById('confirmationWaitingP');
+const confirmationSentP = document.getElementById('confirmationSentP');
+const unconfirmedP = document.getElementById('unconfirmedP');
+const errorInfoP = document.getElementById('errorInfoP');
+const requestEmailConfirmationForm = document.getElementById('requestEmailConfirmationForm');
+document.getElementById('requestEmailConfirmationSubmit').addEventListener('click', async function (e) {
+    e.preventDefault();
+
+    const requestEmailConfirmationForm = document.getElementById('requestEmailConfirmationForm');
+    const formData = new FormData(requestEmailConfirmationForm);
+
+    requestEmailConfirmationForm.style.display = 'none';
+    unconfirmedP.style.display = 'none';
+    confirmationWaitingP.style.display = 'block';
+
+    const response = await fetch(requestEmailConfirmationForm.action, { method: 'POST', body: formData });
+
+    confirmationWaitingP.style.display = 'none';
+
+    if (response.redirected || response.ok) {
+        errorInfoP.style.display = 'none';
+        confirmationSentP.style.display = 'block';
+    }
+    else if (response.status === 409) {
+        confirmationSentP.style.display = 'none';
+        errorInfoP.textContent = await response.text();
+        errorInfoP.style.display = 'block';
+    } else {
+        confirmationSentP.style.display = 'none';
+        errorInfoP.textContent = 'Что-то пошло не так';
+        errorInfoP.style.display = 'block';
+    }
+});
---
+++ src/mvc/Web.MVC/wwwroot/js/user/EditUserProfile/updateUserName.js
@@ -0,0 +1,29 @@
+﻿const userNameModalErrorBlock = document.getElementById('userNameModalErrorBlock');
+document.getElementById('updateUserNameSubmit').addEventListener('click', async function (e) {
+    e.preventDefault();
+
+    const updateUserNameForm = document.getElementById('updateUserNameForm');
+    const formData = new FormData(updateUserNameForm);
+
+    const response = await fetch(updateUserNameForm.action, { method: 'POST', body: formData });
+
+    if (response.ok) {
+        window.location.reload();
+    } else if (response.status === 400 || response.status === 409) {
+        userNameModalErrorBlock.textContent = await response.text();
+        userNameModalErrorBlock.style.display = 'block';
+    } else {
+        userNameModalErrorBlock.textContent = "Что-то пошло не так, попробуйте еще раз";
+        userNameModalErrorBlock.style.display = 'block';
+    }
+});
+
+const userNameModalOverlay = document.getElementById('userNameModalOverlay');
+userNameModalOverlay.addEventListener('click', e => {
+    if (e.target === userNameModalOverlay) {
+        userNameModalOverlay.style.display = 'none';
+    }
+});
+document.getElementById('openUserNameModalBtn').addEventListener('click', () => {
+    userNameModalOverlay.style.display = 'flex';
+});
