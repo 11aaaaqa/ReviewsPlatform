634b3d8893cb18ab09baebf38c1e299c036f43ea
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-02-03 17:36:15 +0400

    Add return model

---
+++ src/microservices/account/AccountMicroservice.Api/Models/ReturnModels/UserReturnModel.cs
@@ -0,0 +1,16 @@
+﻿using AccountMicroservice.Api.Models.Business;
+
+namespace AccountMicroservice.Api.Models.ReturnModels
+{
+    public class UserReturnModel
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool IsEmailVerified { get; set; }
+        public byte[] AvatarSource { get; set; }
+        public bool IsAvatarDefault { get; set; }
+        public DateOnly RegistrationDate { get; set; }
+        public List<Role> Roles { get; set; } = new();
+    }
+}
