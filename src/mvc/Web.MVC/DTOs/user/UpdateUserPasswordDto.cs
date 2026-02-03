011f6bd3ff017552f4b120483ea27aa050c1a98b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-02-03 11:49:39 +0400

    Add password updating via email message

--- src/mvc/Web.MVC/DTOs/user/UpdateUserPasswordDto.cs
+++ src/mvc/Web.MVC/DTOs/user/UpdateUserPasswordDto.cs
@@ -10,17 +10,17 @@ namespace Web.MVC.DTOs.user
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Минимальная длина пароля 8 символов")]
         public string NewPassword { get; set; }
 
-        [Required(ErrorMessage = "Поле \"Старый пароль\" обязательно")]
-        [Display(Name = "Старый пароль")]
-        [DataType(DataType.Password)]
-        [StringLength(100, MinimumLength = 8, ErrorMessage = "Минимальная длина пароля 8 символов")]
-        public string OldPassword { get; set; }
-
         [Required(ErrorMessage = "Поле \"Подтвердите пароль\" обязательно")]
         [Display(Name = "Подтвердите пароль")]
         [DataType(DataType.Password)]
         [Compare(nameof(NewPassword), ErrorMessage = "Пароли не совпадают")]
         [StringLength(100)]
         public string ConfirmNewPassword { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
     }
 }
