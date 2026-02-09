9cc22441ba225cb6f6bd49f3c12504914905529f
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-02-09 10:40:32 +0400

    Add hangfire dashboard authorization

---
+++ src/microservices/account/AccountMicroservice.Api/Services/Hangfire/DashboardAuthorization/AllowAllDashboardAuthorization.cs
@@ -0,0 +1,9 @@
+﻿using Hangfire.Dashboard;
+
+namespace AccountMicroservice.Api.Services.Hangfire.DashboardAuthorization
+{
+    public class AllowAllDashboardAuthorization : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context) => true;
+    }
+}
