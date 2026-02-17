61ca0795a79595184272c00de8bd30d26e21b01e
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-02-17 20:37:28 +0400

    Add models

---
+++ src/microservices/category/CategoryMicroservice.Api/Models/Business/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace CategoryMicroservice.Api.Models.Business
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int ReviewsCount { get; set; } = 0;
+        public List<Subcategory> Subcategories { get; set; }
+    }
+}
---
+++ src/microservices/category/CategoryMicroservice.Api/Models/Business/Subcategory.cs
@@ -0,0 +1,10 @@
+﻿namespace CategoryMicroservice.Api.Models.Business
+{
+    public class Subcategory
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int ReviewsCount { get; set; } = 0;
+        public Guid CategoryId { get; set; }
+    }
+}
