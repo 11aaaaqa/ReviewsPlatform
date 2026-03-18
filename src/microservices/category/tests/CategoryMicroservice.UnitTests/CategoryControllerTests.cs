37108e451f08b9a4e001bdd51e70242829288377
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-03-18 20:50:46 +0400

    Add unit tests

---
+++ src/microservices/category/tests/CategoryMicroservice.UnitTests/CategoryControllerTests.cs
@@ -0,0 +1,120 @@
+﻿using System.Security.Claims;
+using CategoryMicroservice.Api.Controllers;
+using CategoryMicroservice.Api.Models.Business;
+using CategoryMicroservice.Api.Services.CategoryServices;
+using CategoryMicroservice.Api.Services.UnitOfWork;
+using MessageBus.Abstractions;
+using MessageBus.Publisher;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace CategoryMicroservice.UnitTests
+{
+    public class CategoryControllerTests
+    {
+        [Fact]
+        public async Task RemoveCategoryAsync_ReturnsNotFound()
+        {
+            Guid userId = Guid.NewGuid();
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> 
+                { new Claim(ClaimTypes.NameIdentifier, userId.ToString()) }));
+            Guid categoryId = Guid.NewGuid();
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock.Setup(x => x.BeginTransactionAsync());
+            uowMock.Setup(x => x.CategoryRepository.RemoveAsync(categoryId)).Throws<ArgumentException>();
+            var controller = new CategoryController(new Mock<ICategoryRepository<Category>>().Object, uowMock.Object,
+                new Mock<ILogger<CategoryController>>().Object, new Mock<IMessagePublisher>().Object);
+            controller.ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = user } };
+
+            var result = await controller.RemoveCategoryAsync(categoryId);
+
+            Assert.IsType<NotFoundObjectResult>(result);
+            uowMock.Verify(x => x.BeginTransactionAsync());
+            uowMock.Verify(x => x.CategoryRepository.RemoveAsync(categoryId));
+            uowMock.Verify(x => x.RollbackTransactionAsync());
+        }
+
+        [Fact]
+        public async Task RemoveCategoryAsync_ReturnsInternalServerErrorOnCompleting()
+        {
+            Guid userId = Guid.NewGuid();
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                { new Claim(ClaimTypes.NameIdentifier, userId.ToString()) }));
+            Guid categoryId = Guid.NewGuid();
+            var uowMock = new Mock<IUnitOfWork>();
+            uowMock.Setup(x => x.BeginTransactionAsync());
+            uowMock.Setup(x => x.CategoryRepository.RemoveAsync(categoryId));
+            uowMock.Setup(x => x.CompleteAsync()).Throws<Exception>();
+            var controller = new CategoryController(new Mock<ICategoryRepository<Category>>().Object, uowMock.Object,
+                new Mock<ILogger<CategoryController>>().Object, new Mock<IMessagePublisher>().Object);
+            controller.ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = user } };
+
+            var result = await controller.RemoveCategoryAsync(categoryId);
+
+             var methodResult = Assert.IsType<StatusCodeResult>(result);
+             Assert.Equal(500, methodResult.StatusCode);
+             uowMock.Verify(x => x.BeginTransactionAsync());
+             uowMock.Verify(x => x.CategoryRepository.RemoveAsync(categoryId));
+             uowMock.Verify(x => x.CompleteAsync());
+             uowMock.Verify(x => x.RollbackTransactionAsync());
+        }
+
+        [Fact]
+        public async Task RemoveCategoryAsync_ReturnsInternalServerErrorOnPublishing()
+        {
+            Guid userId = Guid.NewGuid();
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                { new Claim(ClaimTypes.NameIdentifier, userId.ToString()) }));
+            Guid categoryId = Guid.NewGuid();
+            var uowMock = new Mock<IUnitOfWork>();
+            var messagePublisherMock = new Mock<IMessagePublisher>();
+            uowMock.Setup(x => x.BeginTransactionAsync());
+            uowMock.Setup(x => x.CategoryRepository.RemoveAsync(categoryId));
+            uowMock.Setup(x => x.CompleteAsync());
+            messagePublisherMock.Setup(x => x.PublishAsync(It.IsAny<MessageBase>())).Throws<Exception>();
+            var controller = new CategoryController(new Mock<ICategoryRepository<Category>>().Object, uowMock.Object,
+                new Mock<ILogger<CategoryController>>().Object, messagePublisherMock.Object);
+            controller.ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = user } };
+
+            var result = await controller.RemoveCategoryAsync(categoryId);
+
+            var methodResult = Assert.IsType<StatusCodeResult>(result);
+            Assert.Equal(500, methodResult.StatusCode);
+            uowMock.Verify(x => x.BeginTransactionAsync());
+            uowMock.Verify(x => x.CategoryRepository.RemoveAsync(categoryId));
+            uowMock.Verify(x => x.CompleteAsync());
+            messagePublisherMock.Verify(x => x.PublishAsync(It.IsAny<MessageBase>()));
+            uowMock.Verify(x => x.RollbackTransactionAsync());
+        }
+
+        [Fact]
+        public async Task RemoveCategoryAsync_ReturnsOk()
+        {
+            Guid categoryId = Guid.NewGuid();
+            Guid userId = Guid.NewGuid();
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                { new Claim(ClaimTypes.NameIdentifier, userId.ToString()) }));
+            var uowMock = new Mock<IUnitOfWork>();
+            var messagePublisherMock = new Mock<IMessagePublisher>();
+            uowMock.Setup(x => x.BeginTransactionAsync());
+            uowMock.Setup(x => x.CategoryRepository.RemoveAsync(categoryId));
+            uowMock.Setup(x => x.CompleteAsync());
+            messagePublisherMock.Setup(x => x.PublishAsync(It.IsAny<MessageBase>()));
+            uowMock.Setup(x => x.CommitTransactionAsync());
+            var controller = new CategoryController(new Mock<ICategoryRepository<Category>>().Object, uowMock.Object,
+                new Mock<ILogger<CategoryController>>().Object, messagePublisherMock.Object);
+            controller.ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = user } };
+
+            var result = await controller.RemoveCategoryAsync(categoryId);
+
+            Assert.IsType<OkResult>(result);
+            uowMock.Verify(x => x.BeginTransactionAsync());
+            uowMock.Verify(x => x.CategoryRepository.RemoveAsync(categoryId));
+            uowMock.Verify(x => x.CompleteAsync());
+            messagePublisherMock.Verify(x => x.PublishAsync(It.IsAny<MessageBase>()));
+            uowMock.Verify(x => x.CommitTransactionAsync());
+        }
+    }
+}
