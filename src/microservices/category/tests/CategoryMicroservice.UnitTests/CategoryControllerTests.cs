460040539256afa87b947436344018e665f4f97a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-03-22 13:35:38 +0400

    Add tests

--- src/microservices/category/tests/CategoryMicroservice.UnitTests/CategoryControllerTests.cs
+++ src/microservices/category/tests/CategoryMicroservice.UnitTests/CategoryControllerTests.cs
@@ -1,5 +1,7 @@
-﻿using System.Security.Claims;
+﻿using System.Runtime.InteropServices.Marshalling;
+using System.Security.Claims;
 using CategoryMicroservice.Api.Controllers;
+using CategoryMicroservice.Api.DTOs.Category;
 using CategoryMicroservice.Api.Models.Business;
 using CategoryMicroservice.Api.Services.CategoryServices;
 using CategoryMicroservice.Api.Services.UnitOfWork;
@@ -117,6 +119,73 @@ namespace CategoryMicroservice.UnitTests
             uowMock.Verify(x => x.CompleteAsync());
             messagePublisherMock.Verify(x => x.PublishAsync(It.IsAny<MessageBase>()));
             uowMock.Verify(x => x.CommitTransactionAsync());
+        }
+
+        [Fact]
+        public async Task UpdateCategoryNameAsync_ReturnsNotFound()
+        {
+            Guid categoryId = Guid.NewGuid();
+            var model = new UpdateCategoryNameDto { Id = categoryId, Name = "Category" };
+            var mock = new Mock<ICategoryRepository<Category>>();
+            mock.Setup(x => x.GetByIdAsync(model.Id)).ReturnsAsync((Category?)null);
+            var controller = new CategoryController(mock.Object, new Mock<IUnitOfWork>().Object,
+                new Mock<ILogger<CategoryController>>().Object, new Mock<IMessagePublisher>().Object);
+
+            var result = await controller.UpdateCategoryNameAsync(model);
+
+            Assert.IsType<NotFoundObjectResult>(result);
+            mock.Verify(x => x.GetByIdAsync(model.Id));
+        }
+
+        [Fact]
+        public async Task UpdateCategoryNameAsync_ReturnsConflict() //не проверено
+        {
+            Guid categoryId = Guid.NewGuid();
+            string newCategoryName = "New";
+            var model = new UpdateCategoryNameDto { Id = categoryId, Name = newCategoryName };
+            var category = new Category { Id = categoryId, Name = "Old" };
+            var mock = new Mock<ICategoryRepository<Category>>();
+            var uowMock = new Mock<IUnitOfWork>();
+            mock.Setup(x => x.GetByIdAsync(model.Id)).ReturnsAsync(category);
+            uowMock.Setup(x => x.CategoryRepository.FindByNameAsync(model.Name))
+                .ReturnsAsync(new Category { Name = model.Name });
+            var controller = new CategoryController(mock.Object, uowMock.Object,
+                new Mock<ILogger<CategoryController>>().Object, new Mock<IMessagePublisher>().Object);
+
+            var result = await controller.UpdateCategoryNameAsync(model);
+
+            Assert.IsType<ConflictObjectResult>(result);
+            mock.Verify(x => x.GetByIdAsync(model.Id));
+            uowMock.Verify(x => x.CategoryRepository.FindByNameAsync(model.Name));
+        }
+
+        [Fact]
+        public async Task UpdateCategoryNameAsync_ReturnsOk()
+        {
+            Guid userId = Guid.NewGuid();
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+                { new Claim(ClaimTypes.NameIdentifier, userId.ToString()) }));
+            Guid categoryId = Guid.NewGuid();
+            string newCategoryName = "New";
+            var model = new UpdateCategoryNameDto { Id = categoryId, Name = newCategoryName };
+            var category = new Category { Id = categoryId, Name = "Old" };
+            var mock = new Mock<ICategoryRepository<Category>>();
+            var uowMock = new Mock<IUnitOfWork>();
+            mock.Setup(x => x.GetByIdAsync(model.Id)).ReturnsAsync(category);
+            uowMock.Setup(x => x.CategoryRepository.FindByNameAsync(model.Name)).ReturnsAsync((Category?)null);
+            uowMock.Setup(x => x.CategoryRepository.Update(category));
+            uowMock.Setup(x => x.CompleteAsync());
+            var controller = new CategoryController(mock.Object, uowMock.Object,
+                new Mock<ILogger<CategoryController>>().Object, new Mock<IMessagePublisher>().Object);
+            controller.ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = user } };
+
+            var result = await controller.UpdateCategoryNameAsync(model);
+
+            Assert.IsType<OkResult>(result);
+            mock.Verify(x => x.GetByIdAsync(model.Id));
+            uowMock.Verify(x => x.CategoryRepository.FindByNameAsync(model.Name));
+            uowMock.Verify(x => x.CategoryRepository.Update(category));
+            uowMock.Verify(x => x.CompleteAsync());
         }
     }
 }
