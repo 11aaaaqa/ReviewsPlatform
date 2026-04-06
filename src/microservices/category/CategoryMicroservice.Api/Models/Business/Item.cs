da31d9909d170eb63bf4aac2420d2a365e869de7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-04-06 20:04:55 +0400

    Remove comments

--- src/microservices/category/CategoryMicroservice.Api/Models/Business/Item.cs
+++ src/microservices/category/CategoryMicroservice.Api/Models/Business/Item.cs
@@ -10,9 +10,9 @@ namespace CategoryMicroservice.Api.Models.Business
         public double GeneralEstimation { get; set; }
         public int ReviewsCount { get; set; }
         public byte[] Picture { get; set; }
-        public Guid SubcategoryId { get; set; } //проверять на существование
+        public Guid SubcategoryId { get; set; }
 
-        public Guid CreatedByUserId { get; set; } //брать из токена
+        public Guid CreatedByUserId { get; set; }
         public ItemStatus Status { get; set; }
         public string? RejectionReason { get; set; }
 
