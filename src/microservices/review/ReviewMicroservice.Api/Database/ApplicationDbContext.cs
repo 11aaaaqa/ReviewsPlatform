62239321165b0ddcc0c6d747cf4c5b5efc84e227
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-04-07 16:29:46 +0400

    Add database

---
+++ src/microservices/review/ReviewMicroservice.Api/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReviewMicroservice.Api.Models.Business;
+
+namespace ReviewMicroservice.Api.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
