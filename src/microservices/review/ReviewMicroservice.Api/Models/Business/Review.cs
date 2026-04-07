d3154c950ccb8ebc829990a92831a3118531abe7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-04-07 16:30:00 +0400

    Add business model

---
+++ src/microservices/review/ReviewMicroservice.Api/Models/Business/Review.cs
@@ -0,0 +1,20 @@
+﻿using ReviewMicroservice.Api.Enums;
+
+namespace ReviewMicroservice.Api.Models.Business
+{
+    public class Review
+    {
+        public Guid Id { get; set; }
+        public string ShortReview { get; set; }
+        public string Text { get; set; }
+        public int ItemEstimation { get; set; }
+        public DateOnly CreatedAt { get; set; }
+        public int LikesCount { get; set; }
+        public int DislikesCount { get; set; }
+        public List<byte[]> Pictures { get; set; } = new();
+        public ReviewStatus ReviewStatus { get; set; }
+
+        public Guid UserId { get; set; }
+        public Guid ItemId { get; set; }
+    }
+}
