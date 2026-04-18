e0a04106a951a62bcf1fd118a3d8ed0d14d9e366
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-04-18 20:35:31 +0400

    Add repositorioes

---
+++ src/microservices/restriction/RestrictionMicroservice.Api/Services/ReportRepository/IReportRepository.cs
@@ -0,0 +1,14 @@
+﻿using RestrictionMicroservice.Api.Models.Business;
+
+namespace RestrictionMicroservice.Api.Services.ReportRepository
+{
+    public interface IReportRepository
+    {
+        Task<Report?> GetByIdAsync(Guid id);
+        Task<List<Report>> GetAllAsync(int pageSize, int pageNumber);
+        Task<List<Report>> GetAllAsync(Guid reportingUserId, int pageSize, int pageNumber);
+        Task AddAsync(Report model);
+        Task RemoveAsync(Guid reportId);
+        Task RemoveAllByReportOnEntityIdAsync(Guid reportOnEntityId);
+    }
+}
---
+++ src/microservices/restriction/RestrictionMicroservice.Api/Services/RestrictionRepository/RestrictionRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestrictionMicroservice.Api.Database;
+using RestrictionMicroservice.Api.Models.Business;
+
+namespace RestrictionMicroservice.Api.Services.RestrictionRepository
+{
+    public class RestrictionRepository(ApplicationDbContext context) : IRestrictionRepository
+    {
+        public async Task<Restriction?> GetByIdAsync(Guid id)
+            => await context.Restrictions.SingleOrDefaultAsync(x => x.Id == id);
+
+        public async Task<Restriction?> GetActiveRestrictionByRestrictedUserIdAsync(Guid restrictedUserId)
+            => await context.Restrictions.Where(x => x.RestrictedUserId == restrictedUserId)
+                .FirstOrDefaultAsync(x => x.ExpiryTime > DateTime.UtcNow);
+
+        public async Task<List<Restriction>> GetAllByRestrictedUserIdAsync(Guid restrictedUserId, int pageNumber,
+            int pageSize)
+            => await context.Restrictions.Where(x => x.RestrictedUserId == restrictedUserId).OrderBy(x => x.CreatedAt)
+                .Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+        public async Task<List<Restriction>> GetAllByRestrictingUserIdAsync(Guid restrictingUserId, int pageNumber, int pageSize)
+            => await context.Restrictions.Where(x => x.RestrictingUserId == restrictingUserId).OrderBy(x => x.CreatedAt)
+                .Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+        public async Task<List<Restriction>> GetAllByRestrictingUserIdAsync(string query, Guid restrictingUserId, int pageNumber, int pageSize)
+            => await context.Restrictions.Where(x => x.RestrictingUserId == restrictingUserId)
+                .Where(x => x.Reason.ToLower().Contains(query.ToLower()))
+                .OrderBy(x => x.CreatedAt)
+                .Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+        public async Task<List<Restriction>> GetAllAsync(int pageNumber, int pageSize)
+            => await context.Restrictions.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+        public async Task<List<Restriction>> GetAllAsync(string query, int pageNumber, int pageSize)
+            => await context.Restrictions.Where(x => x.Reason.ToLower().Contains(query.ToLower()))
+                .Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+        public async Task AddAsync(Restriction restriction)
+        {
+            await context.Restrictions.AddAsync(restriction);
+        }
+
+        public async Task RemoveAsync(Guid restrictionId)
+        {
+            var restriction = await context.Restrictions.SingleOrDefaultAsync(x => x.Id == restrictionId);
+            if (restriction == null)
+                throw new ArgumentException("Restriction with current identifier does not exist");
+
+            context.Restrictions.Remove(restriction);
+        }
+    }
+}
