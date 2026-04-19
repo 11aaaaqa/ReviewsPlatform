e2a60a2018acfc63da311991f0bf491a86615f9c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-04-19 16:11:19 +0400

    Add restriction controller

---
+++ src/microservices/restriction/RestrictionMicroservice.Api/Controllers/RestrictionController.cs
@@ -0,0 +1,153 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RestrictionMicroservice.Api.Constants;
+using RestrictionMicroservice.Api.DTOs.restriction;
+using RestrictionMicroservice.Api.Models.Business;
+using RestrictionMicroservice.Api.Services.UnitOfWork;
+
+namespace RestrictionMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = RoleNames.Admin + "," + RoleNames.Moderator)]
+    public class RestrictionController(IUnitOfWork unitOfWork) : ControllerBase
+    {
+        [HttpGet]
+        [Route("get-by-id/{restrictionId}")]
+        public async Task<IActionResult> GetRestrictionByIdAsync([FromRoute] Guid restrictionId)
+        {
+            var restriction = await unitOfWork.RestrictionRepository.GetByIdAsync(restrictionId);
+            if (restriction == null)
+                return NotFound("Restriction with current identifier does not exist");
+
+            return Ok(restriction);
+        }
+
+        [HttpGet]
+        [Route("all")]
+        public async Task<IActionResult> GetAllRestrictionsAsync(int pageNumber, int pageSize)
+        {
+            var restrictions = await unitOfWork.RestrictionRepository.GetAllAsync(pageNumber, pageSize);
+
+            var restrictionsNextPage = await unitOfWork.RestrictionRepository.GetAllAsync(pageNumber + 1, pageSize);
+            bool isNextPageExisted = restrictionsNextPage.Count > 0;
+
+            return Ok(new RestrictionsResult { Restrictions = restrictions, IsNextPageExisted = isNextPageExisted });
+        }
+
+        [HttpGet]
+        [Route("find")]
+        public async Task<IActionResult> FindRestrictionsAsync(string query, int pageNumber, int pageSize)
+        {
+            var restrictions = await unitOfWork.RestrictionRepository.GetAllAsync(query, pageNumber, pageSize);
+
+            var restrictionsNextPage = await unitOfWork.RestrictionRepository.GetAllAsync(query, pageNumber + 1, pageSize);
+            bool isNextPageExisted = restrictionsNextPage.Count > 0;
+
+            return Ok(new RestrictionsResult { Restrictions = restrictions, IsNextPageExisted = isNextPageExisted });
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("get-active/{userId}")]
+        public async Task<IActionResult> GetActiveRestrictionByUserIdAsync([FromRoute] Guid userId)
+        {
+            var restriction = await unitOfWork.RestrictionRepository.GetActiveRestrictionByRestrictedUserIdAsync(userId);
+            if (restriction == null)
+                return NotFound("User with current identifier does not have any restrictions");
+
+            return Ok(restriction);
+        }
+
+        [HttpGet]
+        [Route("get-by-restricted-user/{userId}")]
+        public async Task<IActionResult> GetAllRestrictionsByRestrictedUserIdAsync([FromRoute] Guid userId, int pageNumber, int pageSize)
+        {
+            var restrictions =
+                await unitOfWork.RestrictionRepository.GetAllByRestrictedUserIdAsync(userId, pageNumber, pageSize);
+
+            var restrictionsNextPage =
+                await unitOfWork.RestrictionRepository.GetAllByRestrictedUserIdAsync(userId, pageNumber + 1, pageSize);
+            bool isNextPageExisted = restrictionsNextPage.Count > 0;
+
+            return Ok(new RestrictionsResult { Restrictions = restrictions, IsNextPageExisted = isNextPageExisted });
+        }
+
+        [HttpGet]
+        [Route("get-by-restricting-user/{userId}")]
+        public async Task<IActionResult> GetAllRestrictionsByRestrictingUserIdAsync([FromRoute] Guid userId, int pageNumber, int pageSize)
+        {
+            var restrictions =
+                await unitOfWork.RestrictionRepository.GetAllByRestrictingUserIdAsync(userId, pageNumber, pageSize);
+
+            var restrictionsNextPage =
+                await unitOfWork.RestrictionRepository.GetAllByRestrictingUserIdAsync(userId, pageNumber + 1, pageSize);
+            bool isNextPageExisted = restrictionsNextPage.Count > 0;
+
+            return Ok(new RestrictionsResult { Restrictions = restrictions, IsNextPageExisted = isNextPageExisted });
+        }
+
+        [HttpGet]
+        [Route("find-by-restricting-user/{userId}")]
+        public async Task<IActionResult> FindRestrictionsByRestrictingUserIdAsync([FromRoute] Guid userId, string query,
+            int pageNumber, int pageSize)
+        {
+            var restrictions =
+                await unitOfWork.RestrictionRepository.GetAllByRestrictingUserIdAsync(query, userId, pageNumber, pageSize);
+
+            var restrictionsNextPage =
+                await unitOfWork.RestrictionRepository.GetAllByRestrictingUserIdAsync(query, userId, pageNumber + 1, pageSize);
+            bool isNextPageExisted = restrictionsNextPage.Count > 0;
+
+            return Ok(new RestrictionsResult { Restrictions = restrictions, IsNextPageExisted = isNextPageExisted });
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> AddRestrictionAsync([FromBody] AddRestrictionDto model)
+        {
+            var activeRestriction = await unitOfWork.RestrictionRepository.GetActiveRestrictionByRestrictedUserIdAsync(model.RestrictedUserId);
+            if (activeRestriction != null)
+                return Conflict("User already has an active restriction");
+
+            string userIdStr = User.Claims.Single(x => x.Type == ClaimTypes.NameIdentifier).Value;
+            Guid userId = new Guid(userIdStr);
+            var restrictionToAdd = new Restriction
+            {
+                Id = Guid.NewGuid(), CreatedAt = DateTime.UtcNow, IsPermanent = model.IsPermanent,
+                Reason = model.Reason, RestrictingUserId = userId,
+                RestrictedUserId = model.RestrictedUserId, RestrictionType = model.RestrictionType,
+                ExpiryTime = model.IsPermanent ? DateTime.UtcNow : DateTime.UtcNow.Add(model.Duration),
+
+                IsDisabled = false, DisablingReason = null, DisabledAt = new DateTime(), DisabledByUserId = Guid.Empty
+            };
+            await unitOfWork.RestrictionRepository.AddAsync(restrictionToAdd);
+            await unitOfWork.CompleteAsync();
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("disable")]
+        public async Task<IActionResult> DisableRestrictionAsync([FromBody] DisableRestrictionDto model)
+        {
+            var restriction = await unitOfWork.RestrictionRepository.GetByIdAsync(model.RestrictionId);
+            if (restriction == null)
+                return NotFound("Restriction with current identifier does not exist");
+
+            string userIdStr = User.Claims.Single(x => x.Type == ClaimTypes.NameIdentifier).Value;
+            Guid userId = new Guid(userIdStr);
+
+            restriction.IsDisabled = true;
+            restriction.DisabledByUserId = userId;
+            restriction.DisabledAt = DateTime.UtcNow;
+            restriction.DisablingReason = model.DisablingReason;
+
+            unitOfWork.RestrictionRepository.Update(restriction);
+            await unitOfWork.CompleteAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ src/microservices/restriction/RestrictionMicroservice.Api/DTOs/restriction/AddRestrictionDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using RestrictionMicroservice.Api.Enums;
+
+namespace RestrictionMicroservice.Api.DTOs.restriction
+{
+    public class AddRestrictionDto
+    {
+        [Required]
+        public Guid RestrictedUserId { get; set; }
+
+        [Required]
+        public RestrictionType RestrictionType { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public bool IsPermanent { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Reason { get; set; }
+    }
+}
