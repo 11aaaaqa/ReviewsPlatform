dab121526e101621f53f69d27fccaad73775ffad
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-04-26 17:51:27 +0400

    Add constants

---
+++ src/mvc/Web.MVC/Constants/StringLengthDtoConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.Constants
+{
+    public class StringLengthDtoConstants
+    {
+        public const int CategoryName = 25;
+        public const int SubcategoryName = 50;
+    }
+}
