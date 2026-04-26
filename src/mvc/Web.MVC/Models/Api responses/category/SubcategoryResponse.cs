2c54ba189e0e25ee007f7e86f97609b36f215f7d
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-04-26 17:51:35 +0400

    Add api response models

---
+++ src/mvc/Web.MVC/Models/Api responses/category/SubcategoryResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.MVC.Models.Api_responses.category
+{
+    public class SubcategoryResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int ReviewsCount { get; set; }
+        public Guid CategoryId { get; set; }
+    }
+}
