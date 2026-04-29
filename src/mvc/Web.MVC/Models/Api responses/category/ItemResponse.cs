929c86b95d776c7648680e801e55a67af24bdeb3
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-04-29 21:00:07 +0400

    Add api response models

---
+++ src/mvc/Web.MVC/Models/Api responses/category/ItemResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace Web.MVC.Models.Api_responses.category
+{
+    public class ItemResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Brand { get; set; }
+        public double GeneralEstimation { get; set; }
+        public int ReviewsCount { get; set; }
+        public byte[] Picture { get; set; }
+        public Guid SubcategoryId { get; set; }
+
+        public ItemStatus Status { get; set; }
+    }
+
+    public enum ItemStatus
+    {
+        Pending,
+        UnderConsideration,
+        Verified
+    }
+}
---
+++ src/mvc/Web.MVC/Models/Api responses/category/ItemsResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.Models.Api_responses.category
+{
+    public class ItemsResult
+    {
+        public List<ItemResponse> Items { get; set; } = new();
+        public bool IsNextPageExisted { get; set; }
+    }
+}
