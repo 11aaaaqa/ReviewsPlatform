5dd67e24f9f85a0d215a416d798b46fc2b487659
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-05-11 17:20:50 +0400

    Add message bus consumers

---
+++ src/microservices/review/ReviewMicroservice.Api/MessageBus/Consumers/SubcategoryRemovedEventConsumer.cs
@@ -0,0 +1,16 @@
+﻿using MessageBus.Handler;
+using MessageBus.Messages.Category;
+using ReviewMicroservice.Api.Services.UnitOfWork;
+
+namespace ReviewMicroservice.Api.MessageBus.Consumers
+{
+    public class SubcategoryRemovedEventConsumer(IUnitOfWork unitOfWork) : IMessageHandler<SubcategoryRemovedEvent>
+    {
+        public async Task HandleAsync(SubcategoryRemovedEvent message)
+        {
+            var reviewsToDelete = await unitOfWork.ReviewRepository.GetByItemIdAsync(message.ItemIdsOfRemovedSubcategory);
+            unitOfWork.ReviewRepository.RemoveRange(reviewsToDelete);
+            await unitOfWork.CompleteAsync();
+        }
+    }
+}
