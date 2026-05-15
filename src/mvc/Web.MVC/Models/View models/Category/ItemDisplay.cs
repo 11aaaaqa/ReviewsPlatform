95f47bbcb5d29ee94578b92e027059609abfb0ad
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-05-15 21:08:32 +0400

    Add model

---
+++ src/mvc/Web.MVC/Models/View models/Category/ItemDisplay.cs
@@ -0,0 +1,13 @@
+﻿namespace Web.MVC.Models.View_models.Category
+{
+    public class ItemDisplay
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Brand { get; set; }
+        public double GeneralEstimation { get; set; }
+        public int ReviewsCount { get; set; }
+        public string PictureSrc { get; set; }
+        public Guid SubcategoryId { get; set; }
+    }
+}
