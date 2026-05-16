b6af08f7389c4f1882c02804c88dcea9cf5750e9
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-05-16 20:35:13 +0400

    Add message bus events

--- src/message-bus/MessageBus/Messages/Category/CategoryRemovedEvent.cs
+++ src/message-bus/MessageBus/Messages/Category/CategoryRemovedEvent.cs
@@ -5,5 +5,6 @@ namespace MessageBus.Messages.Category
     public class CategoryRemovedEvent : MessageBase
     {
         public Guid CategoryId { get; set; }
+        public List<Guid> ItemIdsOfRemovedCategory { get; set; } = new();
     }
 }
