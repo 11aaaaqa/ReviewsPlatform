e87e1ce79290e8990725ddee3a92afba40e58e95
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-05-22 19:57:27 +0400

    Add DTOs

---
+++ src/mvc/Web.MVC/DTOs/reivew/AddReviewDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Web.MVC.Constants;
+
+namespace Web.MVC.DTOs.reivew
+{
+    public class AddReviewDto
+    {
+        [Required(ErrorMessage = "Поле \"Краткое резюме\" обязательно")]
+        [StringLength(StringLengthDtoConstants.ShortReviewMax)]
+        [Display(Name = "Краткое резюме")]
+        public string ShortReview { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Отзыв\" обязательно")]
+        [StringLength(StringLengthDtoConstants.ReviewTextMax)]
+        [Display(Name = "Отзыв")]
+        public string Text { get; set; }
+
+        [Required(ErrorMessage = "Поле \"Оценка\" обязательно")]
+        [Display(Name = "Оценка")]
+        public int ItemEstimation { get; set; }
+
+        [Required]
+        public Guid ItemId { get; set; }
+
+        public List<IFormFile> Pictures { get; set; } = new();
+    }
+}
