9a75eea83aba58935c348a9f7aebc4b2a11da4d2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-05-30 20:22:37 +0400

    Add response model

---
+++ src/mvc/Web.MVC/Models/Api responses/review/ReviewsResultResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.Models.Api_responses.review
+{
+    public class ReviewsResultResponse
+    {
+        public List<ReviewNoPicturesResponse> Reviews { get; set; } = new();
+        public bool IsNextPageExisted { get; set; }
+    }
+}
