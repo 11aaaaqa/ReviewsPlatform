8e38465a6b36df05b7e1e829224929c2e77a5275
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-05-31 20:00:14 +0400

    Add api response models

---
+++ src/mvc/Web.MVC/Models/Api responses/review/ReviewNoPicturesResponse.cs
@@ -0,0 +1,22 @@
+﻿using Web.MVC.Models.Api_responses.review.enums;
+
+namespace Web.MVC.Models.Api_responses.review
+{
+    public class ReviewNoPicturesResponse
+    {
+        public Guid Id { get; set; }
+        public string ShortReview { get; set; }
+        public string Text { get; set; }
+        public int ItemEstimation { get; set; }
+        public DateOnly CreatedAt { get; set; }
+        public int LikesCount { get; set; }
+        public int DislikesCount { get; set; }
+
+        public Guid ItemId { get; set; }
+
+        public Guid UserId { get; set; }
+        public ReviewStatus ReviewStatus { get; set; }
+        public string? RejectionReason { get; set; }
+        public bool IsCreatedWithItem { get; set; }
+    }
+}
---
+++ src/mvc/Web.MVC/Models/Api responses/review/enums/OrderByDate.cs
@@ -0,0 +1,8 @@
+﻿namespace Web.MVC.Models.Api_responses.review.enums
+{
+    public enum OrderByDate
+    {
+        Ascending,
+        Descending
+    }
+}
