b49da3c84981d664d8e6b7b4fde2e42bcedcae08
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-06-10 12:15:25 +0400

    Update script

--- src/mvc/Web.MVC/wwwroot/js/review/get-review-reactions.js
+++ src/mvc/Web.MVC/wwwroot/js/review/get-review-reactions.js
@@ -21,6 +21,27 @@ if (showEmailConfirmationModalOnReaction) {
     });
 } else {
     likeBtn.addEventListener('click', async function () {
+        if (dislikeBtn.classList.contains('is-active')) {
+            dislikeI.classList.remove('fa-solid');
+            dislikeI.classList.add('fa-regular');
+            dislikeBtn.classList.remove('is-active');
+            const dislikesCountSpan = document.getElementById('dislikesCountSpan');
+            dislikesCountSpan.textContent = parseInt(dislikesCountSpan.textContent) - 1;
+        }
+
+        const likesCountSpan = document.getElementById('likesCountSpan');
+        if (likeBtn.classList.contains('is-active')) {
+            likeI.classList.remove('fa-solid');
+            likeI.classList.add('fa-regular');
+            likeBtn.classList.remove('is-active');
+            likesCountSpan.textContent = parseInt(likesCountSpan.textContent) - 1;
+        } else {
+            likeI.classList.remove('fa-regular');
+            likeI.classList.add('fa-solid');
+            likeBtn.classList.add('is-active');
+            likesCountSpan.textContent = parseInt(likesCountSpan.textContent) + 1;
+        }
+
         const reviewId = this.getAttribute('data-reviewId');
         const reactionType = this.getAttribute('data-reactionType');
 
@@ -28,33 +49,33 @@ if (showEmailConfirmationModalOnReaction) {
             {
                 method: 'POST'
             });
-        if (response.redirected || response.ok) {
-            if (dislikeBtn.classList.contains('is-active')) {
-                dislikeI.classList.remove('fa-solid');
-                dislikeI.classList.add('fa-regular');
-                dislikeBtn.classList.remove('is-active');
-                const dislikesCountSpan = document.getElementById('dislikesCountSpan');
-                dislikesCountSpan.textContent = parseInt(dislikesCountSpan.textContent) - 1;
-            }
-
-            const likesCountSpan = document.getElementById('likesCountSpan');
-            if (likeBtn.classList.contains('is-active')) {
-                likeI.classList.remove('fa-solid');
-                likeI.classList.add('fa-regular');
-                likeBtn.classList.remove('is-active');
-                likesCountSpan.textContent = parseInt(likesCountSpan.textContent) - 1;
-            } else {
-                likeI.classList.remove('fa-regular');
-                likeI.classList.add('fa-solid');
-                likeBtn.classList.add('is-active');
-                likesCountSpan.textContent = parseInt(likesCountSpan.textContent) + 1;
-            }
-        } else if (response.status === 401) {
+        if (response.status === 401) {
             const encodedCurrentUrl = document.getElementById('encodedCurrentUrl').value;
             window.location.href = `/account/signin?returnUrl=${encodedCurrentUrl}`;
         }
     });
     dislikeBtn.addEventListener('click', async function () {
+        if (likeBtn.classList.contains('is-active')) {
+            likeI.classList.remove('fa-solid');
+            likeI.classList.add('fa-regular');
+            likeBtn.classList.remove('is-active');
+            const likesCountSpan = document.getElementById('likesCountSpan');
+            likesCountSpan.textContent = parseInt(likesCountSpan.textContent) - 1;
+        }
+
+        const dislikesCountSpan = document.getElementById('dislikesCountSpan');
+        if (dislikeBtn.classList.contains('is-active')) {
+            dislikeI.classList.remove('fa-solid');
+            dislikeI.classList.add('fa-regular');
+            dislikeBtn.classList.remove('is-active');
+            dislikesCountSpan.textContent = parseInt(dislikesCountSpan.textContent) - 1;
+        } else {
+            dislikeI.classList.remove('fa-regular');
+            dislikeI.classList.add('fa-solid');
+            dislikeBtn.classList.add('is-active');
+            dislikesCountSpan.textContent = parseInt(dislikesCountSpan.textContent) + 1;
+        }
+
         const reviewId = this.getAttribute('data-reviewId');
         const reactionType = this.getAttribute('data-reactionType');
 
@@ -62,28 +83,7 @@ if (showEmailConfirmationModalOnReaction) {
             {
                 method: 'POST'
             });
-        if (response.redirected || response.ok) {
-            if (likeBtn.classList.contains('is-active')) {
-                likeI.classList.remove('fa-solid');
-                likeI.classList.add('fa-regular');
-                likeBtn.classList.remove('is-active');
-                const likesCountSpan = document.getElementById('likesCountSpan');
-                likesCountSpan.textContent = parseInt(likesCountSpan.textContent) - 1;
-            }
-
-            const dislikesCountSpan = document.getElementById('dislikesCountSpan');
-            if (dislikeBtn.classList.contains('is-active')) {
-                dislikeI.classList.remove('fa-solid');
-                dislikeI.classList.add('fa-regular');
-                dislikeBtn.classList.remove('is-active');
-                dislikesCountSpan.textContent = parseInt(dislikesCountSpan.textContent) - 1;
-            } else {
-                dislikeI.classList.remove('fa-regular');
-                dislikeI.classList.add('fa-solid');
-                dislikeBtn.classList.add('is-active');
-                dislikesCountSpan.textContent = parseInt(dislikesCountSpan.textContent) + 1;
-            }
-        } else if (response.status === 401) {
+        if (response.status === 401) {
             const encodedCurrentUrl = document.getElementById('encodedCurrentUrl').value;
             window.location.href = `/account/signin?returnUrl=${encodedCurrentUrl}`;
         }
