e0b1817f838ddb70763ba6f0020e7736b57d56c4
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-06-10 11:29:25 +0400

    Add migration

---
+++ src/microservices/review/ReviewMicroservice.Api/Database/Migrations/20260609102807_add_review_reactions_table.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReviewMicroservice.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_review_reactions_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReviewReactions",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReviewId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReactionType = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReviewReactions", x => new { x.ReviewId, x.UserId });
+                    table.ForeignKey(
+                        name: "FK_ReviewReactions_Reviews_ReviewId",
+                        column: x => x.ReviewId,
+                        principalTable: "Reviews",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReviewReactions");
+        }
+    }
+}
