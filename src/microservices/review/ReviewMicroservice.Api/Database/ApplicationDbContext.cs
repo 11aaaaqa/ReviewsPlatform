98a25c105a1cd82864fa2bc0ae4e708321263978
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-06-19 20:14:01 +0400

    Add comment business model

--- src/microservices/review/ReviewMicroservice.Api/Database/ApplicationDbContext.cs
+++ src/microservices/review/ReviewMicroservice.Api/Database/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ReviewMicroservice.Api.Models.Business;
+using ReviewMicroservice.Api.Models.Business.Comments;
 
 namespace ReviewMicroservice.Api.Database
 {
@@ -8,12 +9,19 @@ namespace ReviewMicroservice.Api.Database
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Review> Reviews { get; set; }
+        public DbSet<Comment> Comments { get; set; }
         public DbSet<ReviewReaction> ReviewReactions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<ReviewReaction>().HasKey(x => new { x.ReviewId, x.UserId });
             builder.Entity<ReviewReaction>()
+                .HasOne<Review>()
+                .WithMany()
+                .HasForeignKey(x => x.ReviewId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Comment>()
                 .HasOne<Review>()
                 .WithMany()
                 .HasForeignKey(x => x.ReviewId)
---
+++ src/microservices/review/ReviewMicroservice.Api/Models/Business/Comments/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace ReviewMicroservice.Api.Models.Business.Comments
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Text { get; set; }
+        public Guid? ReplyCommentId { get; set; }
+        public Guid ReviewId { get; set; }
+
+        public CommentStatus CommentStatus { get; set; }
+        public string? RejectionReason { get; set; }
+        public Guid? ConsideredByUserId { get; set; }
+    }
+}
