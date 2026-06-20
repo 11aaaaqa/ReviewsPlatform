47e40aae1e61d01386875f8be2613b3935e57650
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-06-20 19:01:36 +0400

    Add models

---
+++ src/microservices/review/ReviewMicroservice.Api/DTOs/comment/AddCommentDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReviewMicroservice.Api.DTOs.comment
+{
+    public class AddCommentDto
+    {
+        [Required]
+        [StringLength(500)]
+        public string Text { get; set; }
+
+        public Guid? ReplyToCommentId { get; set; }
+
+        [Required]
+        public Guid ReviewId { get; set; }
+    }
+}
---
+++ src/microservices/review/ReviewMicroservice.Api/DTOs/comment/RejectCommentDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReviewMicroservice.Api.DTOs.comment
+{
+    public class RejectCommentDto
+    {
+        [Required]
+        public Guid CommentId { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Reason { get; set; }
+    }
+}
