97cdc6957a2390d4c2b5475f24f752017bfb4d62
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-06-26 15:26:54 +0400

    Refactoring

--- src/mvc/Web.MVC/Controllers/ReviewController.cs
+++ src/mvc/Web.MVC/Controllers/ReviewController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
-using System.Reflection;
 using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
