a5497664c11a837ede95c452ae2e343377b01482
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-06-27 20:01:42 +0400

    Update dtos

--- src/microservices/review/ReviewMicroservice.Api/DTOs/comment/RejectCommentDto.cs
+++ src/microservices/review/ReviewMicroservice.Api/DTOs/comment/RejectCommentDto.cs
@@ -1,14 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Ganss.Xss;
+using System.ComponentModel.DataAnnotations;
 
 namespace ReviewMicroservice.Api.DTOs.comment
 {
     public class RejectCommentDto
     {
+        private static readonly HtmlSanitizer htmlSanitizer = new();
+
         [Required]
         public Guid CommentId { get; set; }
 
         [Required]
         [StringLength(500)]
-        public string Reason { get; set; }
+        public string Reason
+        {
+            get => reason;
+            set => htmlSanitizer.Sanitize(value);
+        }
+        private string reason;
     }
 }
