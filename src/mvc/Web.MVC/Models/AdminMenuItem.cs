ff3ab5156dbaaf88708e8f2a0cad624ccf1362b0
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-07-20 10:35:58 +0400

    Add admin panel view layout

---
+++ src/mvc/Web.MVC/Models/AdminMenuItem.cs
@@ -0,0 +1,10 @@
+﻿namespace Web.MVC.Models
+{
+    public class AdminMenuItem
+    {
+        public string Title { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public bool IsActive { get; set; } = false;
+    }
+}
---
+++ src/mvc/Web.MVC/ViewComponents/AdminSidebarViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.MVC.Models;
+
+namespace Web.MVC.ViewComponents
+{
+    public class AdminSidebarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            const string controllerName = "Admin";
+            var items = new List<AdminMenuItem>
+            {
+                new() { Controller = controllerName, Action = "GetUsersList", Title = "Пользователи" }
+            };
+
+            string currentController = ViewContext.RouteData.Values["controller"]!.ToString()!;
+            string currentAction = ViewContext.RouteData.Values["action"]!.ToString()!;
+
+            var selectedItem = items.SingleOrDefault(x => x.Action == currentAction && x.Controller == currentController);
+            selectedItem?.IsActive = true;
+
+            return View(items);
+        }
+    }
+}
