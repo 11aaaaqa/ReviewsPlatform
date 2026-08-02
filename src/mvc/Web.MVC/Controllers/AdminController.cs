d911bc5956d81f65a5c0e173690836399f919396
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-08-02 13:27:11 +0400

    Add review moderation

--- src/mvc/Web.MVC/Controllers/AdminController.cs
+++ src/mvc/Web.MVC/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Net;
 using System.Text;
 using System.Text.Json;
 using Microsoft.AspNetCore.Authorization;
@@ -6,9 +7,14 @@ using Microsoft.AspNetCore.Mvc;
 using Web.MVC.Constants;
 using Web.MVC.DTOs.admin;
 using Web.MVC.Models.Api_responses.account;
+using Web.MVC.Models.Api_responses.category;
+using Web.MVC.Models.Api_responses.restriction;
 using Web.MVC.Models.Api_responses.review;
 using Web.MVC.Models.View_models.Admin;
+using Web.MVC.Models.View_models.Category;
+using Web.MVC.Models.View_models.Review;
 using Web.MVC.Models.View_models.User;
+using Web.MVC.Services;
 
 namespace Web.MVC.Controllers
 {
@@ -17,11 +23,13 @@ namespace Web.MVC.Controllers
     {
         private readonly ILogger<AdminController> logger;
         private readonly IHttpClientFactory httpClientFactory;
+        private readonly ImageConverter imageConverter;
 
-        public AdminController(ILogger<AdminController> logger, IHttpClientFactory httpClientFactory)
+        public AdminController(ILogger<AdminController> logger, IHttpClientFactory httpClientFactory, ImageConverter imageConverter)
         {
             this.logger = logger;
             this.httpClientFactory = httpClientFactory;
+            this.imageConverter = imageConverter;
         }
 
         [HttpGet]
@@ -101,7 +109,124 @@ namespace Web.MVC.Controllers
         [Route("admin/panel/under-consideration/reviews/{reviewId}")]
         public async Task<IActionResult> GetReviewById([FromRoute] Guid reviewId)
         {
-            return View();
+            HttpClient httpClient = httpClientFactory.CreateClient(HttpClientNameConstants.DefaultWithToken);
+
+            var reviewResponse = await httpClient.GetAsync($"/api/Review/get-by-id/{reviewId}");
+            reviewResponse.EnsureSuccessStatusCode();
+            var review = await reviewResponse.Content.ReadFromJsonAsync<ReviewResponse>();
+
+            var itemResponse = await httpClient.GetAsync($"/api/Item/get-by-id/{review!.ItemId}");
+            itemResponse.EnsureSuccessStatusCode();
+            var item = await itemResponse.Content.ReadFromJsonAsync<ItemResponse>();
+
+            var itemDisplay = new ItemDisplay
+            {
+                Id = item!.Id, Brand = item.Brand, GeneralEstimation = item.GeneralEstimation,
+                Name = item.Name, ReviewsCount = item.ReviewsCount, SubcategoryId = item.SubcategoryId,
+                PictureSrc = imageConverter.GetImageSrc(item.Picture)
+            };
+
+            var userCreatedByResponse = await httpClient.GetAsync($"/api/User/get-user-by-id/{review.UserId}");
+            userCreatedByResponse.EnsureSuccessStatusCode();
+            var userCreatedBy = await userCreatedByResponse.Content.ReadFromJsonAsync<UserResponse>();
+
+            var userCreatedByDisplay = new UserDisplay
+            {
+                Id = userCreatedBy!.Id, Roles = userCreatedBy.Roles, Email = userCreatedBy.Email,
+                IsAvatarDefault = userCreatedBy.IsAvatarDefault, IsEmailVerified = userCreatedBy.IsEmailVerified,
+                RegistrationDate = userCreatedBy.RegistrationDate, UserName = userCreatedBy.UserName,
+                AvatarSrc = imageConverter.GetImageSrc(userCreatedBy.AvatarSource)
+            };
+
+            ReviewDisplay reviewDisplay = new ReviewDisplay
+            {
+                CommentsCount = review.CommentsCount, CreatedAt = review.CreatedAt,
+                DislikesCount = review.DislikesCount,
+                Id = review.Id, ItemEstimation = review.ItemEstimation, LikesCount = review.LikesCount,
+                ShortReview = review.ShortReview, Text = review.Text, IsCreatedWithItem = review.IsCreatedWithItem,
+                Item = itemDisplay, CreatedByUser = userCreatedByDisplay
+            };
+
+            foreach (var picture in review.Pictures)
+            {
+                reviewDisplay.PicturesSrc.Add(imageConverter.GetImageSrc(picture));
+            }
+
+            GetReviewByIdAdminViewModel model = new() { Review = reviewDisplay, IsUserRestricted = false};
+
+            var restrictionResponse = await httpClient.GetAsync($"/api/Restriction/get-active/{userCreatedBy.Id}");
+            if (restrictionResponse.IsSuccessStatusCode)
+            {
+                model.IsUserRestricted = true;
+            }
+            else
+            {
+                if (restrictionResponse.StatusCode != HttpStatusCode.NotFound)
+                    restrictionResponse.EnsureSuccessStatusCode();
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("admin/panel/under-consideration/reviews/{reviewId}/accept")]
+        public async Task<IActionResult> AcceptReview([FromRoute] Guid reviewId)
+        {
+            HttpClient httpClient = httpClientFactory.CreateClient(HttpClientNameConstants.DefaultWithToken);
+
+            var acceptReviewResponse = await httpClient.GetAsync($"/api/Review/accept-review/{reviewId}");
+            acceptReviewResponse.EnsureSuccessStatusCode();
+
+            return RedirectToAction("GetReviews");
+        }
+
+        [HttpPost]
+        [Route("admin/panel/under-consideration/reviews/{reviewId}/reject")]
+        public async Task<IActionResult> RejectReview([FromRoute] Guid reviewId, [FromForm] RejectReviewDto model)
+        {
+            if (model.AddRestriction is { IsPermanent: false, DurationInDays: <= 0})
+                ModelState.AddModelError(string.Empty, "Длительность блокировки не может быть отрицательной или равной 0");
+
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+                return BadRequest(new { errors });
+            }
+
+            HttpClient httpClient = httpClientFactory.CreateClient(HttpClientNameConstants.DefaultWithToken);
+            
+            if (model.AddRestriction != null)
+            {
+                TimeSpan duration = TimeSpan.Zero;
+                if (!model.AddRestriction.IsPermanent)
+                    duration = new TimeSpan(days: model.AddRestriction.DurationInDays, 0, 0, 0);
+                
+                using StringContent jsonContent = new(JsonSerializer.Serialize(new
+                {
+                    ReviewId = reviewId,
+                    model.RejectionReason,
+                    AddRestriction = new
+                    {
+                        model.AddRestriction.RestrictionType,
+                        model.AddRestriction.IsPermanent,
+                        model.AddRestriction.Reason,
+                        Duration = duration
+                    }
+                }), Encoding.UTF8, "application/json");
+
+                var rejectReviewResponse = await httpClient.PutAsync("/api/Review/reject-review", jsonContent);
+                rejectReviewResponse.EnsureSuccessStatusCode();
+            }
+            else
+            {
+                using StringContent jsonContent = new(JsonSerializer.Serialize(new 
+                    { ReviewId = reviewId, model.RejectionReason }), Encoding.UTF8, "application/json");
+
+                var rejectReviewResponse = await httpClient.PutAsync("/api/Review/reject-review", jsonContent);
+                rejectReviewResponse.EnsureSuccessStatusCode();
+            }
+
+            return RedirectToAction("GetReviews");
         }
     }
 }
---
+++ src/mvc/Web.MVC/Models/View models/Admin/GetReviewByIdAdminViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Web.MVC.Models.View_models.Review;
+
+namespace Web.MVC.Models.View_models.Admin
+{
+    public class GetReviewByIdAdminViewModel
+    {
+        public ReviewDisplay Review { get; set; }
+        public bool IsUserRestricted { get; set; }
+    }
+}
