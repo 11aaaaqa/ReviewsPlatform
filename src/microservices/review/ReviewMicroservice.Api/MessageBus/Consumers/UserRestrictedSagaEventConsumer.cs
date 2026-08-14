07cfeff4ee1784fa65efe97686cee8960ec10a5b
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-08-14 17:49:27 +0400

    Add logging

--- src/microservices/review/ReviewMicroservice.Api/MessageBus/Consumers/UserRestrictedSagaEventConsumer.cs
+++ src/microservices/review/ReviewMicroservice.Api/MessageBus/Consumers/UserRestrictedSagaEventConsumer.cs
@@ -6,8 +6,8 @@ using ReviewMicroservice.Api.Services.UnitOfWork;
 
 namespace ReviewMicroservice.Api.MessageBus.Consumers
 {
-    public class UserRestrictedSagaEventConsumer(IUnitOfWork unitOfWork, IMessagePublisher messagePublisher)
-        : IMessageHandler<UserRestrictedSagaEvent>
+    public class UserRestrictedSagaEventConsumer(IUnitOfWork unitOfWork, IMessagePublisher messagePublisher,
+        ILogger<UserRestrictedSagaEventConsumer> logger) : IMessageHandler<UserRestrictedSagaEvent>
     {
         public async Task HandleAsync(UserRestrictedSagaEvent message)
         {
@@ -15,6 +15,8 @@ namespace ReviewMicroservice.Api.MessageBus.Consumers
 
             if (review!.IsCreatedWithItem) 
                 await messagePublisher.PublishAsync(new ReviewCreatedWithItemRejectedEvent { ItemId = review.ItemId });
+
+            logger.LogInformation("SAGA has been successfully completed. Review has been rejected and user has been restricted.");
         }
     }
 }
