ddfc90c396d221533beee7906607768e5b792c49
11aaaaqa <11aaaaqa@users.noreply.github.com>
2026-08-14 17:49:53 +0400

    Update dto

--- src/microservices/review/ReviewMicroservice.Api/DTOs/review/RejectReviewDto.cs
+++ src/microservices/review/ReviewMicroservice.Api/DTOs/review/RejectReviewDto.cs
@@ -1,15 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Ganss.Xss;
+using System.ComponentModel.DataAnnotations;
 
 namespace ReviewMicroservice.Api.DTOs.review
 {
     public class RejectReviewDto
     {
+        private static readonly HtmlSanitizer htmlSanitizer = new();
+
         [Required]
         public Guid ReviewId { get; set; }
 
         [Required]
         [StringLength(500)]
-        public string RejectionReason { get; set; }
+        public string RejectionReason
+        {
+            get => rejectionReason;
+            set => htmlSanitizer.Sanitize(value);
+        }
+        private string rejectionReason;
 
         public AddRestrictionDto? AddRestriction { get; set; }
     }
